f7168c79bf3a7a0973c8febb1546d9ff7551631e
samgoldman <sgoldman216@gmail.com>
2020-05-26 18:28:25 -0600

    Add submission fields

--- src/api.rs
+++ src/api.rs
@@ -8,15 +8,34 @@ use rocket_raw_response::RawResponse;
 use std::io::Write;
 use std::path::PathBuf;
 
+const VALID_SHAPES: [&str; 6] = ["TRIANGLE", "CUBIC", "QUADRATIC", "RECTANGLE", "ELLIPSE", "MIXED"];
+const MAX_IMAGE_SIZE: u64 = 32; // MB
+
+///
+/// Handler for submitting images for processing.
+///
+/// POST Parameters:
+///     image: the image to process (required, max MAX_IMAGE_SIZE MB)
+///     num_shapes: the number of shapes to use for the primitive image (default 500, max 2000)
+///     max_age: Maximum age for each hill climbing attempt (default 100, max 200)
+///     scale_to: The value to scale the image's largest dimension to (default 100)
+///     seed: the random seed. 0 picks a seed based on the time (default 0)
+///     shape: the shape to use. (default TRIANGLE)
+///
 #[post("/submit", data = "<data>")]
-pub fn submit(content_type: &ContentType, data: Data) -> Result<RawResponse, &'static str> {
+pub fn submit(content_type: &ContentType, data: Data) -> JsonValue {
     let request_id = nanoid!(42);
 
     let options = MultipartFormDataOptions::with_multipart_form_data_fields(vec![
         MultipartFormDataField::raw("image")
-            .size_limit(32 * 1024 * 1024)
+            .size_limit(MAX_IMAGE_SIZE * 1024 * 1024)
             .content_type_by_string(Some(mime::IMAGE_STAR))
             .unwrap(),
+        MultipartFormDataField::text("num_shapes"),
+        MultipartFormDataField::text("max_age"),
+        MultipartFormDataField::text("scale_to"),
+        MultipartFormDataField::text("seed"),
+        MultipartFormDataField::text("shape")
     ]);
 
     let mut multipart_form_data = match MultipartFormData::parse(content_type, data, options) {
@@ -24,10 +43,16 @@ pub fn submit(content_type: &ContentType, data: Data) -> Result<RawResponse, &'s
         Err(err) => {
             match err {
                 MultipartFormDataError::DataTooLargeError(_) => {
-                    return Err("The file is too large.");
+                    return json!({
+                        "status": "error",
+                        "message": format!("Image too large ({} MB maximum).", MAX_IMAGE_SIZE)
+                    });
                 }
                 MultipartFormDataError::DataTypeError(_) => {
-                    return Err("The file is not an image.");
+                    return json!({
+                        "status": "error",
+                        "message": "The file is not an image."
+                    });
                 }
                 _ => panic!("{:?}", err),
             }
@@ -48,20 +73,25 @@ pub fn submit(content_type: &ContentType, data: Data) -> Result<RawResponse, &'s
 
             let destination = env::temp_dir().join("primitive_web").join("input").join(request_id.clone() + "." + extension);
             let mut dest_file = fs::File::create(destination).unwrap();
-            let _save_result = dest_file.write_all(data.as_ref());
 
-            Ok(RawResponse::from_vec(data, Some(file_name.clone()), content_type))
+            match dest_file.write_all(data.as_ref()) {
+                Ok(_) => json!(),
+                Err(err) => json!()
+            }
+
         }
-        None => Err("Please input a file."),
+        None => json!({"status": "error",
+            "message": "The image field is required."
+        })
     }
 }
 
-#[get("/is_ready/<request_id>")]
-pub fn is_ready(request_id: String) -> JsonValue {
+#[get("/check_status/<request_id>")]
+pub fn check_status(request_id: String) -> JsonValue {
     json!({
         "request": {
             "type": "GET",
-            "uri": "/is_ready",
+            "uri": "/check_status",
             "parameters": {"request_id": request_id}
         },
         "response": {
