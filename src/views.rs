6020ec3ff64e178f03e1733cf207b3228104dd5e
samgoldman <sgoldman216@gmail.com>
2020-05-27 21:12:33 -0600

    Parameterize default and max values on home page

--- src/views.rs
+++ src/views.rs
@@ -1,18 +1,32 @@
 use rocket_contrib::templates::Template;
 use std::collections::HashMap;
-use crate::VALID_SHAPES;
+use crate::{VALID_SHAPES, MAX_IMAGE_SIZE, NUM_SHAPES_DEFAULT, NUM_SHAPES_MAX, MAX_AGE_DEFAULT, MAX_AGE_MAX, SCALE_TO_DEFAULT, SEED_DEFAULT};
 
 #[derive(Serialize)]
 struct HomeContext {
     page_title: &'static str,
-    shapes: Vec<&'static str>
+    shapes: Vec<&'static str>,
+    max_image_size: u64,
+    default_num_shapes: u32,
+    max_num_shapes: u32,
+    default_max_age: u32,
+    max_max_age: u32,
+    default_scale_to: u32,
+    default_seed: u32
 }
 
 #[get("/home")]
 pub fn home() -> Template {
     let context = HomeContext {
         page_title: "Primitive Web",
-        shapes: VALID_SHAPES.to_vec()
+        shapes: VALID_SHAPES.to_vec(),
+        max_image_size: MAX_IMAGE_SIZE,
+        default_num_shapes: NUM_SHAPES_DEFAULT,
+        max_num_shapes: NUM_SHAPES_MAX,
+        default_max_age: MAX_AGE_DEFAULT,
+        max_max_age: MAX_AGE_MAX,
+        default_scale_to: SCALE_TO_DEFAULT,
+        default_seed: SEED_DEFAULT
     };
 
     Template::render("home", &context)
